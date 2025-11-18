f35f0f3ddd9ffdd2f1a4513e843cdc174b857ed4
Butrint27 <70060142+Butrint27@users.noreply.github.com>
2025-11-18 15:51:13 +0100

    adding grid-list in the HomePage

--- frontend/flavorhub-frontend/src/app/pages/home-page/home-page.ts
+++ frontend/flavorhub-frontend/src/app/pages/home-page/home-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Toolbar } from "../../shared/toolbar/toolbar";
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home-page',
-  imports: [Toolbar],
+  imports: [Toolbar,MatGridListModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
