60e73ddfcef632c778ccb1f0534440a8890a48e0
Butrint27 <70060142+Butrint27@users.noreply.github.com>
2025-11-19 13:45:36 +0100

    adding routerLink to Login and Register in the HomePage

--- frontend/flavorhub-frontend/src/app/pages/home-page/home-page.ts
+++ frontend/flavorhub-frontend/src/app/pages/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Toolbar } from "../../shared/toolbar/toolbar";
 import {MatGridListModule} from '@angular/material/grid-list';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Toolbar,MatGridListModule],
+  imports: [Toolbar, MatGridListModule, RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
