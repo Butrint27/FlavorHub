03be7197657f69da4ade62c6399fd6afd1b8a0f8
Butrint27 <70060142+Butrint27@users.noreply.github.com>
2025-11-21 12:51:54 +0100

    placing the first Card in the MainPage as a first test

--- frontend/flavorhub-frontend/src/app/pages/main-page/main-page.ts
+++ frontend/flavorhub-frontend/src/app/pages/main-page/main-page.ts
@@ -3,10 +3,12 @@ import { ToolbarMenu } from '../../shared/toolbar-menu/toolbar-menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSidenav } from '@angular/material/sidenav';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main-page',
-  imports: [ToolbarMenu, MatSidenavModule, MatPaginatorModule],
+  imports: [ToolbarMenu, MatSidenavModule, MatPaginatorModule, MatCardModule, MatButtonModule],
   templateUrl: './main-page.html',
   styleUrls: ['./main-page.css']
 })
