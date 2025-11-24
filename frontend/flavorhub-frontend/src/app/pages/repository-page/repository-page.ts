a746ae998b44a99874ea902f42f357a412e3c172
Butrint27 <70060142+Butrint27@users.noreply.github.com>
2025-11-24 14:57:46 +0100

    starting with the text,background,and repository to add in the RepositoryPage

--- frontend/flavorhub-frontend/src/app/pages/repository-page/repository-page.ts
+++ frontend/flavorhub-frontend/src/app/pages/repository-page/repository-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ToolbarMenu } from "../../shared/toolbar-menu/toolbar-menu";
 
 @Component({
   selector: 'app-repository-page',
-  imports: [],
+  imports: [ToolbarMenu],
   templateUrl: './repository-page.html',
   styleUrl: './repository-page.css',
 })
