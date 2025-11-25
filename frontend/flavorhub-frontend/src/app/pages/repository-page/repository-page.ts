769be790aa6f448eee7cb16d30439331fd2c6d3c
Butrint27 <70060142+Butrint27@users.noreply.github.com>
2025-11-25 10:53:16 +0100

    compliting the modal form for the RepositoryPage

--- frontend/flavorhub-frontend/src/app/pages/repository-page/repository-page.ts
+++ frontend/flavorhub-frontend/src/app/pages/repository-page/repository-page.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
 import { ToolbarMenu } from "../../shared/toolbar-menu/toolbar-menu";
-import { Chips } from '../../shared/chips/chips';
 
 @Component({
   selector: 'app-repository-page',
   standalone: true,
-  imports: [ToolbarMenu, Chips],
+  imports: [ToolbarMenu],
   templateUrl: './repository-page.html',
   styleUrls: ['./repository-page.css'],
 })
 export class RepositoryPage {
   modalVisible = false;
+  selectedImage: string | ArrayBuffer | null = null;
 
   toggleModal() {
     this.modalVisible = !this.modalVisible;
+    if (!this.modalVisible) this.selectedImage = null; // reset image when modal closes
   }
 
-  
+  onImageSelected(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const file = target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.selectedImage = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
 }
+
 
 
 
